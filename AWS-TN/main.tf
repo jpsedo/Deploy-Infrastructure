4c809600eb12717dc5856d41ac9fd3ed1fc171dd
jpsedo <jpsedo@gmail.com>
2022-12-01 23:39:19 -0600

    Update main.tf

--- AWS-TN/main.tf
+++ AWS-TN/main.tf
@@ -14,9 +14,24 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0f4cf4474622ba9dd"
-  instance_type = "t2.micro"
+resource "aws_iam_role" "k8s_masters_role" {
+  count              = var.enabled ? 1 : 0
+  name               = "k8s_masters_role_${var.cluster_name}"
+  assume_role_policy = <<EOP
+{
+  "Version": "2012-10-17",
+  "Statement": [
+   {
+     "Effect": "Allow",
+     "Principal": {
+        "Service": "eks.amazonaws.com"   
+     },
+     "Action": "sts:AssumeRole"  
+   }     
+  ]    
+}
+EOP
+  tags               = var.tags
 }
 
 
